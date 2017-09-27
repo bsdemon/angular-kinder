ee7e767a46b413177264080f9960ebde251e4679
Ilia Iliev <ilia@magicsolutions.bg>
2017-09-27 20:17:03 +0300

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LeftSideBarComponent } from './components/left-side-bar/left-side-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSideBarComponent,
+    NavBarComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule
